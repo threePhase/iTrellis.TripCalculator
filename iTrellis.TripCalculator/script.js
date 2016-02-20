55a6d42df293aa1587fefd61c85ebe0b74119ac8
Michael Snider <snidemic@gmail.com>
2016-02-19 16:47:47 -0800

    Update sample page to use all controller functions

--- iTrellis.TripCalculator/script.js
+++ iTrellis.TripCalculator/script.js
@@ -8,6 +8,20 @@ function printTransaction(transaction) {
     return transaction.Owner + ' - Expense: $' + transaction.Amount;
 }
 
+function deleteTransaction() {
+    var id = $('#deleteId').val();
+    $.ajax({
+        type: "DELETE",
+        url: endpoint + '/' + id,
+        success: function (data) {
+            $('.deleteResult').text("Successfully deleted Transaction: " + printTransaction(data))
+        },
+        dataType: 'json'
+    }).fail(function (jqXHR, textStatus, err) {
+        $('.deleteResult').text('Error: ' + err);
+    });
+}
+
 function getAllTransactions() {
     $.getJSON(endpoint)
         .done(function (data) {
@@ -63,5 +77,43 @@ function lookupByOwner() {
         .fail(function (jqXHR, textStatus, err) {
             $('<li>', { text: 'Error: ' + err })
                 .appendTo($('.transactionsByOwner'));
+        });
+}
+
+function updateTransaction() {
+    var id = $('#updateId').val();
+    var amount = $('#amountUpdate').val();
+    var owner = $('#ownerUpdate').val();
+    var updateTransaction = {
+        "Id": id,
+        "Amount": amount,
+        "Owner": owner,
+    };
+    $.ajax({
+        type: "PUT",
+        url: endpoint + '/' + id,
+        data: updateTransaction,
+        success: function () {
+            $('.updateResult').text("Successfully updated ID: " + id)
+        },
+        dataType: 'json'
+    }).fail(function (jqXHR, textStatus, err) {
+        $('.updateResult').text('Error: ' + err);
+    });
+}
+
+function addTransaction() {
+    var amount = $('#amountAdd').val();
+    var owner = $('#ownerAdd').val();
+    var transaction = {
+        "Amount": amount,
+        "Owner": owner,
+    };
+    $.post(endpoint, transaction)
+        .done(function (data) {
+            $('.addResult').text(printTransaction(data))
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('.addResult').text('Error: ' + err);
         });
 }
